3c7647195a77068f25ad664aecbeee71ed9b6f72
seonb2n <626ksb@gmail.com>
2021-04-25 18:03:52 +0900

    각각의 cardView 에 대한 클릭 이벤트 구현

--- app/src/main/java/com/example/findrestaurant2/MainActivity.java
+++ app/src/main/java/com/example/findrestaurant2/MainActivity.java
@@ -3,15 +3,15 @@ package com.example.findrestaurant2;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private ArrayList<MyData> myDataset;
 
@@ -28,11 +28,19 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         myDataset = new ArrayList<>();
-        mAdapter = new MyAdapter(myDataset);
+        final MyAdapter mAdapter = new MyAdapter(myDataset);
+
+        mAdapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Toast.makeText(getApplicationContext(), mAdapter.getText(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
         mRecyclerView.setAdapter(mAdapter);
 
-        myDataset.add(new MyData("Spaghetti", R.mipmap.spaghetti));
-        myDataset.add(new MyData("Spaghetti", R.mipmap.spaghetti));
-        myDataset.add(new MyData("Spaghetti", R.mipmap.spaghetti));
+        myDataset.add(new MyData("Spaghetti1", R.mipmap.spaghetti));
+        myDataset.add(new MyData("Spaghetti2", R.mipmap.spaghetti));
+        myDataset.add(new MyData("Spaghetti3", R.mipmap.spaghetti));
     }
 }
--- app/src/main/java/com/example/findrestaurant2/MyAdapter.java
+++ app/src/main/java/com/example/findrestaurant2/MyAdapter.java
@@ -6,6 +6,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -13,19 +15,34 @@ import java.util.ArrayList;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     private ArrayList<MyData> mDataset;
 
+    private OnItemClickListener mListener;
+
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder{
         // each data item is just a string in this case
         public ImageView mImageView;
         public TextView mTextView;
+        public CardView cv;
 
-        public ViewHolder(View view) {
+        public ViewHolder(final View view){
             super(view);
             mImageView = (ImageView)view.findViewById(R.id.card_view_image_view);
             mTextView = (TextView)view.findViewById(R.id.card_view_text_view);
+            cv = (CardView)view.findViewById(R.id.card_view);
+
+            cv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mListener != null) {
+                        mListener.onItemClick(view, getAdapterPosition());
+                    }
+                }
+            });
+
         }
+
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
@@ -59,6 +76,19 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public int getItemCount() {
         return mDataset.size();
     }
+
+    public String getText(int position) {
+        return mDataset.get(position).text;
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(View v, int position);
+    }
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.mListener = listener;
+    }
+
+
 }
 
 class MyData{
