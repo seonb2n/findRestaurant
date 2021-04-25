a5aba715d0690c5d52952832952e57c8b98214c1
seonb2n <626ksb@gmail.com>
2021-04-25 11:05:08 +0900

    recyclerView 를 활용해서 card View 화면에 횡적으로 띄우는 기능 구현

--- app/src/main/java/com/example/findrestaurant2/MainActivity.java
+++ app/src/main/java/com/example/findrestaurant2/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.findrestaurant2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<MyData> myDataset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler1);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        myDataset = new ArrayList<>();
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+        myDataset.add(new MyData("Spaghetti", R.mipmap.spaghetti));
+        myDataset.add(new MyData("Spaghetti", R.mipmap.spaghetti));
+        myDataset.add(new MyData("Spaghetti", R.mipmap.spaghetti));
     }
 }
